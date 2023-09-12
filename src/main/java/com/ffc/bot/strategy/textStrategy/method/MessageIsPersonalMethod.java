b91bde9acae2cb5c37b8db75d503ba9a8c8c89da
kirgo <kirgotop@gmail.com>
2023-09-12 11:06:04 +0300

    fixed bug with duplicate queues in the database && added catching of all commands sent to private messages

---
+++ src/main/java/com/ffc/bot/strategy/textStrategy/method/MessageIsPersonalMethod.java
@@ -0,0 +1,33 @@
+package main.java.com.ffc.bot.strategy.textStrategy.method;
+
+import main.java.com.ffc.bot.responseTextModule.ResponseTextBuilder;
+import main.java.com.ffc.bot.responseTextModule.TextFormat;
+import main.java.com.ffc.bot.responseTextModule.defaultResponse.DefaultQueueResponse;
+import main.java.com.ffc.bot.specialMessage.PersonalSendMessage;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.List;
+
+public class MessageIsPersonalMethod implements StrategyMethod {
+
+    private String userId;
+
+    public MessageIsPersonalMethod(String userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public List<BotApiMethod> getResponse(Update update, SendMessage response, String chatId) {
+        PersonalSendMessage personalSendMessage = new PersonalSendMessage();
+        personalSendMessage.setChatId(userId);
+
+        personalSendMessage.setText(
+                new ResponseTextBuilder()
+                        .addText(DefaultQueueResponse.CANT_USE_THIS_COMMAND_HERE, TextFormat.Monocular)
+                        .get()
+        );
+        return List.of(personalSendMessage);
+    }
+}
